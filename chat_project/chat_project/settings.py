c853d26c914ebd34bd38a5f3742b04aac63237b1
Ainomic Dev <contact@ainomic.in>
2024-07-24 12:29:09 +0530

    Configure channel layer in settings

--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -124,5 +124,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Daphne
+# Channels[daphne]
 ASGI_APPLICATION = "chat_project.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
